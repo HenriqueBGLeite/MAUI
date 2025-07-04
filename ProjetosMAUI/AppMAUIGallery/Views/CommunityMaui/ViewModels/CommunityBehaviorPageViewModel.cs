923bde42683af814ff741f6ab4b79ad824dc4817
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-07-04 18:55:24 -0300

    feat: atualiza repositorio

--- ProjetosMAUI/AppMAUIGallery/Views/CommunityMaui/ViewModels/CommunityBehaviorPageViewModel.cs
+++ ProjetosMAUI/AppMAUIGallery/Views/CommunityMaui/ViewModels/CommunityBehaviorPageViewModel.cs
@@ -7,16 +7,22 @@ using System.Windows.Input;
 
 namespace AppMAUIGallery.Views.CommunityMaui.ViewModels
 {
-    public class CommunityBehaviorPageViewModel
+    public class CommunityBehaviorPageViewModel : BindableObject
     {
         public ICommand PressedCommand { get; set; }
+        public ICommand MyCommand { get; }
 
         public CommunityBehaviorPageViewModel()
         {
             PressedCommand = new Command(Pressed);
+            MyCommand = new Command(OnMyCommand);
         }
 
         private void Pressed()
+        {
+            App.Current.MainPage.DisplayAlert("Fui pressionado!", "Fui pressionado!", "OK");
+        }
+        private void OnMyCommand()
         {
             App.Current.MainPage.DisplayAlert("Fui pressionado!", "Fui pressionado!", "OK");
         }
